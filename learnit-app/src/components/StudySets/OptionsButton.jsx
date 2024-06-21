ac322a7594687dc0643e79c67debcbecd2426f57
Levente Nagy <nagylevi0503@gmail.com>
2024-06-21 11:49:18 +0200

    OptionsButton functionality update

--- learnit-app/src/components/StudySets/OptionsButton.jsx
+++ learnit-app/src/components/StudySets/OptionsButton.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 
 import { SlOptions } from "react-icons/sl";
 
-const OptionsButton = () => {
+const OptionsButton = ({ onClick }) => {
 
     const [isHovered, setIsHovered] = useState(false);
 
@@ -12,6 +13,7 @@ const OptionsButton = () => {
     className={`w-10 h-10 flex justify-center items-center rounded-full duration-500 cursor-pointer ${isHovered ? 'bg-slate-600' : ''}`}
     onMouseEnter={() => setIsHovered(true)}
     onMouseLeave={() => setIsHovered(false)}
+    onClick={onClick}
 >
     <SlOptions className={`w-6 h-6 ${isHovered ? 'text-accent_green_dark' : 'text-cstm_white'}`}/>
 </div>
--- learnit-app/src/components/StudySets/RightPanel.jsx
+++ learnit-app/src/components/StudySets/RightPanel.jsx
@@ -12,6 +12,7 @@ const RightPanel = () => {
 
   const [data, setData] = useState([]);
   const [hoverStates, setHoverStates] = useState({});
+  const [isOptionsOpen, setIsOptionsOpen] = useState({});
 
   useEffect(() => {
     axios.get('/data')
@@ -43,18 +44,23 @@ const RightPanel = () => {
     setHoverStates((prevHoverStates) => ({ ...prevHoverStates, [itemName]: false }));
   };
 
+  const handleOptionsClick = (itemName) => {
+    setIsOptionsOpen((prevIsOptionsOpen) => ({...prevIsOptionsOpen, [itemName]:!prevIsOptionsOpen[itemName] }));
+  };
+
   return (
     <div className='w-[70%] flex justify-center items-center relative z-10 font-poppins'>
 
         <div className='w-[90%] h-[90%] flex flex-col justify-center items-center bg-gradient-to-br from-white/30 to-slate-600/30 backdrop-blur-md rounded-[40px] shadow-2xl'>
 
             <div className='w-full h-24 flex justify-center items-center rounded-t-[40px]'>
+              <div className='w-full flex items-center justify-center'>
                 <h1 className='w-fit text-5xl font-medium text-cstm_white select-none z-10'>Your Study Sets</h1>
+              </div>
 
-            </div>
-        
-            <div className='w-full h-12 flex justify-end items-center px-5'>
-              <DeleteButton />
+              <div className='w-fit h-12 flex justify-end items-center gap-6 pr-6'>
+                <DeleteButton />
+              </div>
             </div>
             
             <div className='w-full h-full flex flex-col rounded-b-[40px] px-4'>
@@ -72,13 +78,21 @@ const RightPanel = () => {
                     <h2 className={`text-[28px] font-medium ${hoverStates[item.name] ? 'text-accent_green_dark' : 'text-cstm_white'}`}>{item.name}</h2>
                   </div>
 
-                  <div className='flex justify-center items-center gap-10'>
+                  <div className='flex justify-center items-center gap-10 relative'>
                     <div className='w-14 flex justify-center items-center relative'>
                       <p className='text-cstm_bg_dark text-lg text-center font-semibold flex items-center justify-center select-none rounded-lg px-3 relative z-10'>{item.questions.length}</p>
                       <IoChatbox className={`w-12 h-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[45%] text-slate-300 z-0`}/>
                     </div>
 
-                    <OptionsButton />
+                    <OptionsButton data-item-name={item.name} onClick={() => handleOptionsClick(item.name)} />
+
+                    <div className={`w-[250px] h-32 flex absolute top-0 right-0 bg-slate-600 z-20 ${isOptionsOpen[item.name]? 'block' : 'hidden'} options-menu`}>
+                      {isOptionsOpen[item.name] && (
+                        <div>
+                          {/* Options menu content goes here */}
+                        </div>
+                      )}
+                    </div>
                   </div>
 
                 </div>
