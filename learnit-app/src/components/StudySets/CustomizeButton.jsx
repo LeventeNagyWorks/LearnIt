b8d59bf9cbcdbedb0960a912a011f6db9ede5bc5
Levente Nagy <nagylevi0503@gmail.com>
2024-07-21 19:57:49 +0200

    CustomizeButton design update

--- learnit-app/src/components/StudySets/CustomizeButton.jsx
+++ learnit-app/src/components/StudySets/CustomizeButton.jsx
@@ -6,11 +6,11 @@ import { FiEdit } from "react-icons/fi";
 const CustomizeButton = ({ index, isHovered, setHoveredItem }) => {
   return (
     <div 
-        className={`w-8 h-8 flex justify-center items-center p-1 rounded-md cursor-pointer ${isHovered ? 'bg-accent_green_dark' : ''}`}
+        className={`w-8 h-8 flex justify-center items-center p-1 rounded-md cursor-pointer duration-500 ${isHovered ? 'bg-accent_green_dark' : ''}`}
         onMouseEnter={() => setHoveredItem(index)}
         onMouseLeave={() => setHoveredItem(null)}
     >
-        <FiEdit className={`w-full h-full ${isHovered ? 'text-cstm_bg_dark' : 'text-gray-400'}`}/>
+        <FiEdit className={`w-full h-full duration-500 ${isHovered ? 'text-cstm_bg_dark' : 'text-gray-400'}`}/>
     </div>
   )
 }
