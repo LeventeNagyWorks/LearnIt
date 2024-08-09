786b3450e7cf945f9263e400d020bd3be341a42f
Levente Nagy <nagylevi0503@gmail.com>
2024-08-09 13:01:44 +0200

    "Updated HeroSection component: changed CSS classes and styles, added HeroSectionVisuals component, and modified text content and layout."

--- learnit-app/src/routes/HeroSection.jsx
+++ learnit-app/src/routes/HeroSection.jsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import LoadingScreen from '../components/LoadingScreen';
 import { isLoading } from '../signals';
 import { useSignals } from '@preact/signals-react/runtime';
+import HeroSectionVisuals from '../components/HeroSectionVisuals';
 
 const HeroSection = () => {
 
@@ -14,17 +15,23 @@ const HeroSection = () => {
 
   return (
     <>
+
             <div 
-                className="h-screen w-screen font-poppins flex justify-end items-center bg-cover overflow-hidden"
-                style={{ backgroundImage: `url(${darkHatImage})` }}
+                className="h-screen w-screen font-poppins flex justify-end items-end bg-cstm_bg_dark overflow-hidden"
             >
 
-                <div className="w-[60%] h-[90%] flex flex-col justify-start items-center px-8 py-36 gap-20">
+                <div className='w-full self-start px-12'>
+                    <h1 className="text-cstm_white lg:text-[100px] md:text-[110px] mb-14 select-none self-end font-poetsen">Learn <span className="text-accent_green_dark">It</span></h1>
+                </div>
 
-                    <div className="flex flex-col justify-between gap-5">
-                        <h1 className="text-black lg:text-[150px] md:text-[110px] mb-14 select-none self-end font-poetsen">Learn <span className="text-accent_green_dark">It</span></h1>
-                        <p className="text-white lg:text-6xl select-none font-poppins">Learn everything you want.</p>
-                        <p className="text-white lg:text-5xl select-none self-end">Let me help you.</p>
+                <HeroSectionVisuals />
+
+                <div className="w-[45%] h-full flex flex-col justify-evenly items-center px-8 py-36">
+
+                    <div className="flex flex-col justify-between items-end">
+                        <p className="text-white lg:text-[53px] text-right select-none font-poppins self-end">LEARN EVERYTHING</p>
+                        <p className="text-white lg:text-[92px] text-right select-none font-poppins self-end">YOU WANT</p>
+                        <p className="text-white lg:text-[61px] select-none self-end">LET ME HELP YOU</p>
                     </div>
                 
                     <Link
