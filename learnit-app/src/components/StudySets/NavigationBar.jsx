e2192cbcb3866419634adbeee8c0caff7323da09
Levente Nagy <nagylevi0503@gmail.com>
2024-09-15 12:31:55 +0200

    Little design changes.

--- learnit-app/src/components/StudySets/NavigationBar.jsx
+++ learnit-app/src/components/StudySets/NavigationBar.jsx
@@ -16,7 +16,7 @@ const NavigationBar = () => {
   return (
     <div className='w-[92%] h-[12%] min-h-[12%] flex flex-col justify-center font-poppins font-semibold text-cstm_white z-20'>
       <div className='w-full h-[65%] min-h-[65%] flex justify-between items-center bg-zinc-400/40 backdrop-blur-md rounded-[34px] px-8'>
-          <p className='lg:text-[42px] select-none'>Welcome back, <span className='text-accent_green_dark'>{username ? ` ${username}` : ''}</span>!</p>
+          <p className='lg:text-[38px] select-none'>Welcome back, <span className='text-accent_green_dark'>{username ? ` ${username}` : ''}</span>!</p>
           <div className='w-12 h-12 bg-slate-700 rounded-full cursor-pointer border-2 border-slate-700 hover:border-cstm_white'>
           </div>
       </div>
