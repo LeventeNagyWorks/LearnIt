b51c11479ea96987c76bf70e51536b52939e029a
Levente Nagy <nagylevi0503@gmail.com>
2024-09-17 11:49:25 +0200

    Added profile dropdown menu with settings and friends options to NavigationBar component

--- learnit-app/src/components/StudySets/NavigationBar.jsx
+++ learnit-app/src/components/StudySets/NavigationBar.jsx
@@ -24,7 +24,23 @@ const NavigationBar = () => {
     <div className='w-[92%] h-[12%] min-h-[12%] flex flex-col justify-center font-poppins font-semibold text-cstm_white z-20'>
       <div className='w-full h-[65%] min-h-[65%] flex justify-between items-center bg-zinc-400/40 backdrop-blur-md rounded-[34px] px-8'>
           <p className='lg:text-[38px] select-none'>Welcome back, <span className='text-accent_green_dark2'>{username ? ` ${username}` : ''}</span>!</p>
-          <div className='flex'>
+          <div className='flex justify-center items-center relative'>
+
+            <div className={`absolute -bottom-[310%] w-[250px] flex flex-col justify-center items-center gap-4 bg-gradient-to-br from-green-600 to-green-900 rounded-2xl z-40 duration-500 px-5 py-5 ${isProfileFocused.value ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+
+              <svg className='absolute -top-[16px] right-[62px]' width="30" height="20" viewBox="0 0 150 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M75 0L150 100H0L75 0Z" fill="#168640"/>
+              </svg>
+
+              <div className='w-full flex justify-center items-center hover:bg-green-600 border-2 border-cstm_white px-4 py-1 rounded-lg cursor-pointer duration-500'>
+                <p className=''>Profile Settings</p>
+              </div>
+              <div className='w-full flex justify-center items-center hover:bg-green-600 border-2 border-cstm_white px-4 py-1 rounded-lg cursor-pointer duration-500'>
+                <p className=''>Friends</p>
+              </div>
+              
+            </div>
+
             <div className='flex justify-center items-center px-2 overflow-hidden'>
               <p className={`lg:text-2xl duration-500 select-none ${isProfileFocused.value ? 'translate-x-0' : 'translate-x-[110%]'}`}>{username}</p>
             </div>
