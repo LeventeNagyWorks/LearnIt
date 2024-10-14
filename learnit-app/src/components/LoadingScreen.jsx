1e4ac8971e977e29b03bdeb8abee29db36a1fb10
Levente Nagy <nagylevi0503@gmail.com>
2024-10-14 11:40:25 +0200

    Tried to add a RiveComponent

--- learnit-app/src/components/LoadingScreen.jsx
+++ learnit-app/src/components/LoadingScreen.jsx
@@ -4,6 +4,7 @@
 import React, { useEffect } from 'react'
 import { isLoadingEnabled } from '../signals';
 import { useSignal } from '@preact/signals-react';
+import { useRive } from 'rive-react';
 
 function LoadingScreen() {
   useSignal();
@@ -11,37 +12,22 @@ function LoadingScreen() {
   useEffect(() => {
     setTimeout(() => {
       isLoadingEnabled.value = true;
-    }, 2000);
+    }, 3500);
   }, []);
+
+  const { RiveComponent } = useRive({
+    src: "light_bulb.riv",
+    animations: "timeline",
+    autoplay: true,
+    onLoadError: () => console.log("ERROR LOADING RIVE"),
+    onLoad: () => console.log("LOADED RIVE"),
+  });
 
   return (
     <div className={`absolute w-full h-screen bg-cstm_bg_dark flex justify-center items-center font-poppins font-medium text-cstm_white text-5xl select-none duration-500`}>
       <div className='w-full flex flex-col justify-center items-center'>
         <h1 className="text-cstm_white text-[150px] mb-14 select-none font-poetsen">Learn <span className="text-accent_green_dark">It</span></h1>
-        <div className='w-[200px] h-[200px] relative'>
-          <svg className="w-full h-full" viewBox="0 0 100 100">
-            <circle
-              cx="50"
-              cy="50"
-              r="45"
-              fill="none"
-              stroke="#1a4d01"
-              strokeWidth="3"
-            />
-            <circle
-              cx="50"
-              cy="50"
-              r="45"
-              fill="none"
-              stroke="#20CC00"
-              strokeWidth="3"
-              strokeLinecap="round"
-              strokeDasharray="50 231.7433388230814"
-              className="origin-center animate-loadingSpin rotate-[60deg]"
-            />
-          </svg>
-          <span className='absolute top-1/2 left-1/2 w-[50%] h-[50%] bg-gradient-to-br from-green-500 to-green-900 rounded-full transform -translate-x-1/2 -translate-y-1/2'></span>
-        </div>
+        <RiveComponent />
       </div>
     </div>
   )
