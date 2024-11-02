6cbae23a6e0126294e22164d8b9df8fd49bcf4cb
Levente Nagy <nagylevi0503@gmail.com>
2024-11-02 19:02:46 +0100

    Updated HeroSection component with changes to layout, styling, and responsive design

--- learnit-app/src/routes/HeroSection.jsx
+++ learnit-app/src/routes/HeroSection.jsx
@@ -42,17 +42,17 @@ const HeroSection = () => {
   return (
     <>
             <div 
-                className="h-screen w-screen font-poppins flex justify-start items-center overflow-hidden flex-grow"
+                className="h-screen w-screen font-poppins flex flex-col md:flex-row lg:flex-row justify-start items-center overflow-hidden flex-grow"
                 style={{ backgroundImage: `url(${Background})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
             >
 
-                <div className='w-1/2 h-full flex flex-col justify-evenly items-center px-8'>
+                <div className='w-full md:w-1/2 h-full flex flex-col justify-evenly items-center px-8'>
                     <div className='w-full h-fit flex justify-center items-center gap-10'>
-                        <img src={Logo} alt="" />
-                        <h1 className="text-cstm_white text-[96px] select-none font-poetsen">Learn <span className="text-accent_green_dark">It</span></h1>
+                        <img src={Logo} alt="" className='w-16 lg:w-24'/>
+                        <h1 className="text-cstm_white text-[60px] md:text-[96px] select-none font-poetsen">Learn <span className="text-accent_green_dark">It</span></h1>
                     </div>
                     
-                    <p className='font-poppins font-medium text-center text-4xl mx-24 select-none text-cstm_white'>
+                    <p className='font-poppins font-medium text-center text-2xl md:text-4xl md:mx-24 select-none text-cstm_white'>
                         Turn your handouts into study sets and make learning
                         <span className='text-accent_green_dark'>{text}</span>
                         <Cursor 
@@ -64,13 +64,13 @@ const HeroSection = () => {
 
                     <button
                         onClick={handleButtonClick}
-                        className="w-fit h-fit text-accent_green_dark hover:text-slate-950 font-medium hover:bg-accent_green_dark text-3xl border-2 border-accent_green_dark rounded-xl px-12 py-2 duration-700 select-none hover:shadow-[0_0_36px_rgba(40,255,0,0.7)]"
+                        className="w-fit h-fit text-slate-950 md:text-accent_green_dark md:hover:text-slate-950 font-medium bg-accent_green_dark md:bg-transparent md:hover:bg-accent_green_dark text-2xl md:text-3xl border-2 border-accent_green_dark rounded-xl px-12 py-2 duration-700 select-none shadow-[0_0_36px_rgba(40,255,0,0.7)] md:shadow-none md:hover:shadow-[0_0_36px_rgba(40,255,0,0.7)]"
                     >
                         {isLoggedIn.value ? `Go to Study Sets` : 'Getting Started'}
                     </button>
                 </div>
 
-                <div className='w-1/2 h-full grid grid-rows-8 grid-cols-2 gap-8 px-[5%] py-[10%]'>
+                <div className='w-full md:w-1/2 h-full grid grid-rows-8 grid-cols-2 gap-8 px-[5%] py-[10%]'>
                     <HeroSectionElements isLarge={false} bgImg={Book} text={"Create Study Sets"} />
                     <HeroSectionElements isLarge={true} bgImg={BoyLearning} text={"Learning Is For Every Age"} />
                     <HeroSectionElements isLarge={true} bgImg={GirlReading} text={"Unlock Achievements"} />
